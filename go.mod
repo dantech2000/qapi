28aaba290bf3be4cfe27925f555339465b218fc9
Daniel Rodriguez <drodriguez@codecademy.com>
2023-03-15 08:11:29 -0700

    Add support for output format, syntax highlighting, & more

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/dantech2000/qapi
 go 1.19
 
 require (
+	github.com/alecthomas/chroma v0.10.0
 	github.com/fatih/color v1.14.1
+	github.com/gocarina/gocsv v0.0.0-20230226133904-70c27cb2918a
 	github.com/spf13/cobra v1.6.1
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
--- main.go
+++ main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/alecthomas/chroma/quick"
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "qapi",
-	Short: "A command line tool to fetch data from an API",
-	Run:   qapi,
-}
-
-var apiURL string
-var flagNoColor bool
-var apiKey string
+var (
+	apiURL      string
+	apiKey      string
+	format      string
+	flagNoColor bool
+	rootCmd     = &cobra.Command{
+		Use:   "qapi",
+		Short: "A command line tool to fetch data from an API",
+		Run:   qapi,
+	}
+)
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&apiURL, "url", "u", "", "The API URL to fetch data from")
-	rootCmd.MarkPersistentFlagRequired("url")
-	rootCmd.PersistentFlags().BoolVar(&flagNoColor, "no-color", false, "This flag disables color output to stdout")
 	rootCmd.PersistentFlags().StringVarP(&apiKey, "key", "k", "", "This flag attaches an API Key header to the HTTP Request")
+	rootCmd.PersistentFlags().StringVarP(&format, "output", "o", "json", "This flag set the output format")
+	rootCmd.PersistentFlags().BoolVar(&flagNoColor, "no-color", false, "This flag disables color output to stdout")
+	rootCmd.MarkFlagRequired("url")
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 		os.Exit(1)
 	}
 }
@@ -37,7 +44,7 @@ func main() {
 func qapi(cmd *cobra.Command, args []string) {
 	req, err := http.NewRequest("GET", apiURL, nil)
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatal("Error:", err)
 		return
 	}
 	if apiKey != "" {
@@ -47,18 +54,100 @@ func qapi(cmd *cobra.Command, args []string) {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatal("Error: No URL Provided ", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error:", err)
+		log.Fatal("Error:", err)
 		return
 	}
 
 	if flagNoColor {
 		color.NoColor = true
 	}
-	color.Green(string(body))
+
+	switch format {
+	case "json":
+		printJSONOutput(body)
+	case "yaml":
+		printYAMLOutput(body)
+	// case "csv":
+	// TODO: Fix function printCSVOutput(body)
+	default:
+		fmt.Println("Error: unsupported output format")
+	}
 }
+
+func printJSONOutput(body []byte) {
+	var data interface{}
+	err := json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatal("Error:", err)
+		return
+	}
+
+	prettyJSON, err := json.MarshalIndent(data, "", "    ")
+	if err != nil {
+		log.Fatal("Error:", err)
+		return
+	}
+
+	// Colorize and print the output
+	quick.Highlight(os.Stdout, string(prettyJSON), "json", "terminal256", "native")
+}
+
+func printYAMLOutput(body []byte) {
+	var data interface{}
+	err := yaml.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Pretty print the output
+	prettyYAML, err := yaml.Marshal(data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Colorize and print the output
+	quick.Highlight(os.Stdout, string(prettyYAML), "yaml", "terminal256", "native")
+}
+
+// func printCSVOutput(body []byte) {
+// 	var data []map[string]interface{}
+// 	err := json.Unmarshal(body, &data)
+// 	if err != nil {
+// 		log.Fatal("Error:", err)
+// 		return
+// 	}
+
+// 	// Convert data to CSV format
+// 	csvOutput, err := gocsv.MarshalString(data)
+// 	if err != nil {
+// 		log.Fatal("Error:", err)
+// 		return
+// 	}
+
+// 	// Replace "," with ";" to avoid conflicts with CSV formatting
+// 	csvOutput = strings.ReplaceAll(csvOutput, ",", ";")
+
+// 	// Colorize and print the output
+// 	csvReader := csv.NewReader(strings.NewReader(csvOutput))
+// 	records, err := csvReader.ReadAll()
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		return
+// 	}
+
+// 	// Colorize and print the output
+// 	for _, record := range records {
+// 		for _, field := range record {
+// 			color.Blue(field)
+// 		}
+// 		fmt.Println()
+// 	}
+// }
